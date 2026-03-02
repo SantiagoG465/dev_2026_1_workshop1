6490eacf52ee8b6cc6e385df8be87019cc178ef4
Santiago Guzman <santm099@gmail.com>
2026-03-02 13:32:46 -0500

    factorial

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -70,9 +70,10 @@ class Magic:
     
     def factorial(self, n):
        if n < 0:
-           raise    ValueError("El número debe ser no negativo")
-       if n == 0 or n == 1:
+           return None
+       if   n == 0 or n == 1:
            return 1
+       resultado = 1
        for i in range(2, n + 1):
            resultado *= i
            return resultado
