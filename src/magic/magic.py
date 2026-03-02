1115f372d5e90dd18be82459f81a4e6974bb2565
Santiago Guzman <santm099@gmail.com>
2026-03-02 13:01:02 -0500

    fibonacci

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -1,15 +1,16 @@
 class Magic:
-
     def fibonacci(self, n):
         if n <= 0:
             return []
         if n == 1:  
             return [0]
+        
         sequence = [0, 1]
-        while   len(sequence) < n:
+        while len(sequence) < n:
             next_value = sequence[-1] + sequence[-2]
             sequence.append(next_value)
-            return sequence
+        
+        return sequence  # Asegúrate de que el return esté alineado con el while
     
     def secuencia_fibonacci(self, n):
         if n <= 0:
