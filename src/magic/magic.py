1738f60d1ec36af62f8f00ec3e17a947a65108cd
Santiago Guzman <santm099@gmail.com>
2026-03-02 13:04:07 -0500

    es primo self

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -29,11 +29,15 @@ class Magic:
     def es_primo(self, n):
         if n <= 1:
             return False
-        for i in range(2, int(n**0.5) + 1):
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, int(n**0.5) + 1, 2):
             if n % i == 0:
-                return False
-        return True
-    
+                return False    
+            return True 
+        
     def generar_primos(self, n):
         primos = []
         num = 2
